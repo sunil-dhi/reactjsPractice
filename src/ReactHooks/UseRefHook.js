4ecb89d3b30ee639989cf68f4b70dc305b3e5ff8
sunil-dhi <sunildhimanmatharu@gmail.com>
2023-07-23 17:24:34 -0400

    useRef DOM elemet access directly implemented

--- src/ReactHooks/UseRefHook.js
+++ src/ReactHooks/UseRefHook.js
@@ -1,32 +1,51 @@
-// The useRef Hook allows you to persist values between renders.
+// // The useRef Hook allows you to persist values between renders.
 
-// It can be used to store a mutable value that does not cause a re-render when updated.
+// // It can be used to store a mutable value that does not cause a re-render when updated.
 
-// It can be used to access a DOM element directly.
+// // It can be used to access a DOM element directly.
 
-import React, { useEffect, useRef, useState } from "react";
+// import React, { useEffect, useRef, useState } from "react";
 
-const UseRefHook = () => {
-  const [data, setData] = useState("hello");
+// const UseRefHook = () => {
+//   const [data, setData] = useState("hello");
 
-    const count=useRef(0)
-//   const [count, setCount] = useState(0);
-  useEffect(() => {
-//     setCount(count + 1);                          ///problem is infinite loop re-rendering now solutin is use useRef hook
-         count.current=count.current+1                                           
-   });
+//     const count=useRef(0)
+// //   const [count, setCount] = useState(0);
+//   useEffect(() => {
+// //     setCount(count + 1);                          ///problem is infinite loop re-rendering now solutin is use useRef hook
+//          count.current=count.current+1                                           
+//    });
 
-  return (
-    <div>
-      <input type="text"  value={data} onChange={(e) => setData(e.target.value)} />
-      <p>number of time it renders:{count.current}</p>
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <input type="text"  value={data} onChange={(e) => setData(e.target.value)} />
+//       <p>number of time it renders:{count.current}</p>
+//     </div>
+//   );
+// };
 
-export default UseRefHook;                   
+// export default UseRefHook;                   
 
 
 //created a mutable variable count using useRef which hinders re-rendering process.
 
+// 2nd usecase of useRef hook is we can access DOM elemenrt directly
 
+import React, { useRef, useState } from 'react'
+
+const UseRefHook = () => {
+    const [data,setData]=useState('');
+    const user=useRef('')
+
+    const clickHandler=()=>{
+        user.current.style.backgroundColor="black"
+    }
+  return (
+    <div>
+         <input type="text" ref={user} value={data} onChange={(e)=>setData(e.target.value)} />
+         <button onClick={clickHandler}>click</button>
+    </div>
+  )
+}
+
+export default UseRefHook
