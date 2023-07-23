47ba011997827fa96bfec778b741273a1d4ae768
sunil-dhi <sunildhimanmatharu@gmail.com>
2023-07-23 17:23:41 -0400

    useRef hook pratice

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 import Usestatehook from './ReactHooks/UseStateHook'
 import UseEffectHook from './ReactHooks/UseEffectHook'
 import UseContextHook from './ReactHooks/usecontext/UseContextHook'
+import UseRefHook from './ReactHooks/UseRefHook'
 function App() {
   return (
     <div className="App">
       <Usestatehook />
       <UseEffectHook />
       <UseContextHook />
-
+      <UseRefHook />
     </div>
   );
 }
---
+++ src/ReactHooks/UseRefHook.js
@@ -0,0 +1,32 @@
+// The useRef Hook allows you to persist values between renders.
+
+// It can be used to store a mutable value that does not cause a re-render when updated.
+
+// It can be used to access a DOM element directly.
+
+import React, { useEffect, useRef, useState } from "react";
+
+const UseRefHook = () => {
+  const [data, setData] = useState("hello");
+
+    const count=useRef(0)
+//   const [count, setCount] = useState(0);
+  useEffect(() => {
+//     setCount(count + 1);                          ///problem is infinite loop re-rendering now solutin is use useRef hook
+         count.current=count.current+1                                           
+   });
+
+  return (
+    <div>
+      <input type="text"  value={data} onChange={(e) => setData(e.target.value)} />
+      <p>number of time it renders:{count.current}</p>
+    </div>
+  );
+};
+
+export default UseRefHook;                   
+
+
+//created a mutable variable count using useRef which hinders re-rendering process.
+
+
